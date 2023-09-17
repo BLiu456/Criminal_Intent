43aa1db5427c279812b7b4fecb8ee6bc87289923
Nub456 <benjaminliu456@gmail.com>
2023-09-16 22:38:20 -0700

    Finished app up to Ch12 but have not tested

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,6 +1,12 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
-data class Crime(val id: UUID, val title: String, val date: Date, val isSolved: Boolean)
+data class Crime(
+    @PrimaryKey val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
